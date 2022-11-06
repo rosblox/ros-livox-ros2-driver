6f192c0d85e651d4affb71b2b4ea6017c2b484e0
Max Polzin <max.polzin@epfl.ch>
2022-11-06 09:41:51 +0100

    Update workdir.

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it --net=host --volume $(pwd)/livox_ros2_driver:/colcon_ws/src/livox_ros2_driver ghcr.io/rosblox/ros-livox-ros2-driver:foxy
+docker run -it --net=host --workdir=/colcon_ws --volume $(pwd)/livox_ros2_driver:/colcon_ws/src/livox_ros2_driver ghcr.io/rosblox/ros-livox-ros2-driver:foxy
