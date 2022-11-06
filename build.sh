ece40a784dd47cf275ada42ca2a4425793f8d077
Max <max@seervision.com>
2022-11-06 10:23:07 +0100

    Build for any architecture.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker build --progress string  -t ghcr.io/rosblox/ros-livox-ros2-driver:foxy --build-arg ROS_DISTRO=foxy --platform  linux/arm64 .
+docker build --progress string  -t ghcr.io/rosblox/ros-livox-ros2-driver:foxy --build-arg ROS_DISTRO=foxy .
