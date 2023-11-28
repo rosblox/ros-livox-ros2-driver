04a3249b0b35318ef916aa6b3486d73226981544
Max Polzin <polzin.max@gmail.com>
2023-11-28 09:43:32 +0100

    Remove unused build arg.

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ROS_DISTRO=humble
-
 REPOSITORY_NAME="$(basename "$(dirname -- "$( readlink -f -- "$0"; )")")"
 
-docker build --progress=plain --build-arg ROS_DISTRO=${ROS_DISTRO} -t ghcr.io/rosblox/${REPOSITORY_NAME}:${ROS_DISTRO} .
+docker build --progress=plain -t ghcr.io/rosblox/${REPOSITORY_NAME}:humble .
